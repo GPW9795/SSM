493406930e748b38e6dcfddfc7f074d4b3b7e2d1
gaopeiwen <vivian9795@163.com>
2020-12-02 14:16:53 +0800

    mbg

---
+++ MyBatis/6.MyBatis_mbg/src/test/java/MyBatisCRUDTest.java
@@ -0,0 +1,22 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+
+public class MyBatisCRUDTest {
+    SqlSessionFactory sqlSessionFactory;
+
+    @Before
+    public void init() throws IOException {
+        String resource = "mybatis_config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+}
